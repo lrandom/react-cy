4648983ca8d881806497ae8e3edddf6543cd3547
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-11-18 10:47:21 +0700

    update code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ComponentA from "./components/ComponentA.jsx";
+
+function App() {
+    return (
+        <>
+            <ComponentA/>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/ComponentA.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+class ComponentA extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    increaseCount() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    componentDidMount() {
+        console.log('Component A did mount');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Component A did update');
+    }
+
+    componentWillUnmount() {
+        console.log('Component A will unmount")');
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <h1>Component A</h1>
+                <p>{this.state.count}</p>
+                <button onClick={() => this.increaseCount()}>Increase
+                </button>
+            </div>
+        );
+    }
+}
+
+export default ComponentA;
